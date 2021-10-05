a8729bc4b3c8a464c226384cc0d692b9515ee0b0
David <david.llobet6@gmail.com>
2021-10-05 18:22:06 +0200

    Write general function with exceptions

---
+++ strictEquals.jest.js
@@ -0,0 +1,18 @@
+function strictEquals(a, b) {
+	if (isNaN(a) && isNaN(b)) {
+        return false;
+	}
+    else if (Object.is(a, 0) && Object.is(b, -0)) {
+        return true;
+    }
+    else if (Object.is(a, -0) && Object.is(b, 0)) {
+        return true;
+    }
+    else if (Object.is(a, b)) {
+        return true;
+    }
+    else {
+        return false;
+    }    	
+}
+
